42277f3b04c95843e9f90e3046234e7a3a2aea74
Hello1Robot <hello11robot@gmail.com>
2023-11-17 16:19:53 +0900

    스티커 드래그 기능 추가

--- sticker/main.js
+++ sticker/main.js
@@ -17,29 +17,36 @@ createStickerBtn.addEventListener("click", () => {
     // 타이틀 추가
     const itemTitle = document.createElement("h1");
     itemTitle.innerText = sticker.title;
+    itemTitle.style.display = "inline";
     item.appendChild(itemTitle);
+
+    const btnDiv = document.createElement("div");
 
     //생성 버튼 추가
     const createItemBtn = document.createElement("button");
     createItemBtn.innerText = "항목 추가";
 
     createItemBtn.addEventListener("click", (e) => {
-        const id = +(e.target.parentNode.id.replace("Sticker", ""));
+        const id = +(e.target.parentNode.parentNode.id.replace("Sticker", ""));
         const sticker = getSticker(id);
         sticker.createStickerItem();
     })
 
-    item.appendChild(createItemBtn);
+    btnDiv.appendChild(createItemBtn);
+
+
 
     // 삭제 버튼 추가
     const deleteBtn = document.createElement("button");
     deleteBtn.innerText = "스티커 삭제";
     deleteBtn.addEventListener("click", (event) => {
-        const sticker = event.target.parentNode;
+        const sticker = event.target.parentNode.parentNode;
         sticker.remove();
     });
 
-    item.appendChild(deleteBtn);
+    btnDiv.appendChild(deleteBtn);
+
+    item.appendChild(btnDiv);
 
     // Sticker Contents 추가
     const stickerContents = document.createElement("div");
@@ -48,6 +55,36 @@ createStickerBtn.addEventListener("click", () => {
     item.appendChild(stickerContents);
 
     stickerField.appendChild(item);
+
+
+    // // 움직이게 하기
+    item.addEventListener("mousedown", (e) => {
+        const sticker = e.target;
+        let shiftX = e.clientX - sticker.getBoundingClientRect().left;
+        let shiftY = e.clientY - sticker.getBoundingClientRect().top;
+
+
+        sticker.style.zIndex = 1000;
+
+        moveAt(e.pageX, e.pageY);
+
+        function moveAt(pageX, pageY) {
+            sticker.style.left = pageX - shiftX + "px";
+            sticker.style.top = pageY - shiftY + "px";
+        }
+
+        function onMouseMove(event) {
+            moveAt(event.pageX, event.pageY);
+        }
+
+        document.addEventListener("mousemove", onMouseMove);
+
+        sticker.onmouseup = function () {
+            document.removeEventListener("mousemove", onMouseMove);
+            sticker.onmouseup = null;
+        };
+    });
+
 
 })
 
